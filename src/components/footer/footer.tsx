9ea1e25c88ff355f0a07c5b0deb6a997488e48ac
katmakhan <jibinvictorjohn191@gmail.com>
2024-10-23 21:07:55 +0530

    footer added with rights reserved

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,24 +5,33 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white py-12 px-4 md:px-6">
       <div className="container mx-auto">
-        {/* Wrapper for supabase logo and social icons */}
-        <div className="flex flex-col md:flex-row items-center justify-between mb-8">
-          <Link href="/" className="flex items-center mb-6 md:mb-0">
+       {/* Wrapper for Optionxi logo and social icons */}
+        <div className="flex flex-col items-center md:items-start mb-8">
+        {/* OptionXi Logo */}
+        <Link href="/" className="flex items-center mb-4">
             <svg className="w-8 h-8 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21 8V16C21 18.7614 18.7614 21 16 21H8C5.23858 21 3 18.7614 3 16V8C3 5.23858 5.23858 3 8 3H16C18.7614 3 21 5.23858 21 8Z" fill="#3ECF8E"/>
+            <path d="M21 8V16C21 18.7614 18.7614 21 16 21H8C5.23858 21 3 18.7614 3 16V8C3 5.23858 5.23858 3 8 3H16C18.7614 3 21 5.23858 21 8Z" fill="#3ECF8E" />
             </svg>
             <span className="text-2xl font-bold">OptionXi</span>
-          </Link>
-          <div className="flex space-x-4">
-            <Link href="#" aria-label="Twitter"><Twitter className="w-6 h-6 text-gray-400 hover:text-white" /></Link>
-            <Link href="#" aria-label="GitHub"><Github className="w-6 h-6 text-gray-400 hover:text-white" /></Link>
-            <Link href="#" aria-label="Discord">
-              <svg className="w-6 h-6 text-gray-400 hover:text-white" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
-              </svg>
+        </Link>
+
+        {/* Social Icons */}
+        <div className="flex space-x-4">
+            <Link href="#" aria-label="Twitter">
+            <Twitter className="w-6 h-6 text-gray-400 hover:text-white" />
             </Link>
-            <Link href="#" aria-label="YouTube"><Youtube className="w-6 h-6 text-gray-400 hover:text-white" /></Link>
-          </div>
+            <Link href="#" aria-label="GitHub">
+            <Github className="w-6 h-6 text-gray-400 hover:text-white" />
+            </Link>
+            <Link href="#" aria-label="Discord">
+            <svg className="w-6 h-6 text-gray-400 hover:text-white" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
+            </svg>
+            </Link>
+            <Link href="#" aria-label="YouTube">
+            <Youtube className="w-6 h-6 text-gray-400 hover:text-white" />
+            </Link>
+        </div>
         </div>
 
         {/* Grid section for rest of the links */}
@@ -51,6 +60,15 @@ export default function Footer() {
               ))}
             </ul>
           </div>
+        </div>
+
+        {/* Separator */}
+        <hr className="my-8 border-gray-600" />
+
+        {/* Footer bottom section */}
+        <div className="flex flex-col md:flex-row items-center justify-between text-gray-400">
+          <p>&copy; 2024 OptionXi. All rights reserved.</p>
+          <p className="mt-4 md:mt-0">Built with ❤️ by the traders community.</p>
         </div>
       </div>
     </footer>
