0b0270836860506cc3b5347fed36e181094671bd
katmakhan <jibinvictorjohn191@gmail.com>
2024-11-12 15:05:10 +0530

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,25 +6,30 @@ import Navbar from '@/components/navbar/navbar';
 import Footer from '@/components/footer/footer';
 
 
+
 export const metadata: Metadata = {
   metadataBase: new URL('https://optionxi.com'),
   title: 'OptionXi App',
-  description: 'Option Xi is the future of virtual trading, with live scanners and price alerts, also integration live algo trading with past portfolio',
+  description: 'OptionXi is the future of virtual trading, with live scanners and price alerts, also integration live algo trading with past portfolio',
   icons: {
     icon: '/favicon.ico',
-    apple:  [
-      '/metadata/iphone-touch/apple-touch-icon-60x60.png',
-      '/metadata/iphone-touch/apple-touch-icon-76x76.png',
-      '/metadata/iphone-touch/apple-touch-icon-120x120.png',
-      '/metadata/iphone-touch/apple-touch-icon-152x152.png',
+    apple: [
+      '/metadata/apple-touch-icon-57x57.png',
+      '/metadata/apple-touch-icon-72x72.png',
+      '/metadata/apple-touch-icon-76x76.png',
+      '/metadata/apple-touch-icon-114x114.png',
+      '/metadata/apple-touch-icon-120x120.png',
+      '/metadata/apple-touch-icon-144x144.png',
+      '/metadata/apple-touch-icon-152x152.png',
+      '/metadata/apple-touch-icon-180x180.png',
     ],
   },
   openGraph: {
     type: 'website',
     url: 'https://optionxi.com',
-    siteName:'OptionXi',
-    title:  'OptionXi',
-    description: 'The future of virtual trading, with live screeners and price alerts',
+    siteName: 'OptionXi',
+    title: 'OptionXi',
+    description: 'The future of virtual trading, screeners, alerts, heatmaps, AI stock summary',
     images: [
       {
         url: '/metadata/opengraph-image.jpg',
@@ -37,16 +42,21 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     site: 'https://optionxi.com',
-    title:  'OptionXi',
+    title: 'OptionXi',
     creator: 'Jibin Victor John',
     description: 'The future of virtual trading, with live screeners and price alerts',
     images: [
       {
         url: '/metadata/twitter-image.jpg',
+        width: 1200,
+        height: 628,
+        alt: 'OptionXi Twitter Card Image',
       },
     ],
   }
 };
+
+
 
 export default function RootLayout({
   children,
